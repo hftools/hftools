fcd5fcb0c42b44d81493d8311dac9355e484ee6d
Jörgen Stenarson <jorgen.stenarson@kroywen.se>
2014-02-27 17:25:30 +0100

    pep8. Remove redundant cast to tuple

--- hftools/file_formats/hdf5/v_02.py
+++ hftools/file_formats/hdf5/v_02.py
@@ -9,7 +9,7 @@
 try:
     import h5py
     import_failed = False
-except ImportError: # pragma: no cover
+except ImportError:  # pragma: no cover
     import_failed = True
     h5py = None
 
@@ -21,17 +21,20 @@ from hftools.dataset import DimRep, DimSweep, hfarray, DataBlock,\
 dimrep = {"DimRep": DimRep,
           "DimSweep": DimSweep,
           "DimMatrix_i": DimMatrix_i,
-          "DimMatrix_j": DimMatrix_j,}
+          "DimMatrix_j": DimMatrix_j,
+          }
 
 
 def getvar(db, key):
     X = db[key]
 
-
     dims = []
     for x in X.dims:
         if len(x):
-            dim = dimrep[x[0].attrs.get("dimtype", "DimSweep")](x[0].name.strip("/"), x[0][...], unit=x[0].attrs.get("unit", None))
+            dimcls = dimrep[x[0].attrs.get("dimtype", "DimSweep")]
+            dim = dimcls(x[0].name.strip("/"),
+                         x[0][...],
+                         unit=x[0].attrs.get("unit", None))
             dims.append(dim)
 
     unit = X.attrs.get("unit", None)
@@ -39,10 +42,12 @@ def getvar(db, key):
     if len(dims) == len(X.dims):
         return hfarray(X, dims=dims, unit=unit, outputformat=outputformat)
     elif len(X.dims) == 1 and "dimtype" in X.attrs:
-        dim = dimrep[X.attrs.get("dimtype", "DimSweep")](X.name.strip("/"), X[...], unit=unit)
+        dimcls = dimrep[X.attrs.get("dimtype", "DimSweep")]
+        dim = dimcls(X.name.strip("/"), X[...], unit=unit)
         return hfarray(X, dims=(dim,), unit=unit)
     else:
         return np.array(X)
+
 
 def read_hdf5(filehandle, **kw):
     db = DataBlock()
@@ -50,20 +55,19 @@ def read_hdf5(filehandle, **kw):
         db[k] = getvar(filehandle, k)
     return db
 
+
 def save_hdf5(db, filehandle, **kw):
     filehandle.attrs["hftools file version"] = "0.2"
     for k, v in db.ivardata.items():
         data = v.data
         if data.ndim:
             if data.dtype.name.startswith("datetime64"):
-                datadtype = data.dtype.name
                 data = data.astype(np.uint64)
-            dset = filehandle.create_dataset(k, data=data)
+            filehandle.create_dataset(k, data=data)
         else:
             if data.dtype.name.startswith("datetime64"):
-                datadtype = data.dtype.name
                 data = data.astype(np.uint64)
-            dset = filehandle.create_dataset(k, data=data)
+            filehandle.create_dataset(k, data=data)
         filehandle[k].attrs["dimtype"] = v.__class__.__name__
         if v.unit:
             filehandle[k].attrs["unit"] = v.unit
@@ -73,14 +77,12 @@ def save_hdf5(db, filehandle, **kw):
     for k, data in db.vardata.items():
         if data.ndim:
             if data.dtype.name.startswith("datetime64"):
-                datadtype = data.dtype.name
                 data = data.astype(np.uint64)
-            dset = filehandle.create_dataset(k, data=data)
+            filehandle.create_dataset(k, data=data)
         else:
             if data.dtype.name.startswith("datetime64"):
-                datadtype = data.dtype.name
                 data = data.astype(np.uint64)
-            dset = filehandle.create_dataset(k, data=data)
+            filehandle.create_dataset(k, data=data)
         filehandle[k].attrs["arraytype"] = data.__class__.__name__
         if data.unit:
             filehandle[k].attrs["unit"] = data.unit
