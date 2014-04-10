cb7d9357bd60659cf85576cf7b4f3f538e6c4ec3
Jörgen Stenarson <jorgen.stenarson@kroywen.se>
2014-04-10 12:56:46 +0200

    Improve unicode handling in hdf5 format

--- hftools/file_formats/hdf5/v_02.py
+++ hftools/file_formats/hdf5/v_02.py
@@ -15,13 +15,14 @@ except ImportError:  # pragma: no cover
 
 import numpy as np
 from hftools.dataset import DimRep, DimSweep, hfarray, DataBlock,\
-    DimMatrix_i, DimMatrix_j
-
+    DimMatrix_i, DimMatrix_j, DimPartial
+from hftools.py3compat import PY3
 
 dimrep = {"DimRep": DimRep,
           "DimSweep": DimSweep,
           "DimMatrix_i": DimMatrix_i,
           "DimMatrix_j": DimMatrix_j,
+          "DimPartial": DimPartial,
           }
 
 
@@ -39,14 +40,32 @@ def getvar(db, key):
 
     unit = X.attrs.get("unit", None)
     outputformat = X.attrs.get("outputformat", None)
+    dtype = X.attrs.get("dtype", None)
     if len(dims) == len(X.dims):
-        return hfarray(X, dims=dims, unit=unit, outputformat=outputformat)
+        return hfarray(X.value, dims=dims, unit=unit,
+                       dtype=dtype, outputformat=outputformat)
     elif len(X.dims) == 1 and "dimtype" in X.attrs:
         dimcls = dimrep[X.attrs.get("dimtype", "DimSweep")]
         dim = dimcls(X.name.strip("/"), X[...], unit=unit)
-        return hfarray(X, dims=(dim,), unit=unit)
+        return hfarray(X.value, dims=(dim,), dtype=dtype, unit=unit)
     else:
-        return np.array(X)
+        return np.array(X.value, dtype=dtype)
+
+
+def create_dataset(db, key, data, expandable=False):
+    data_dtype = data.dtype
+    if data.ndim:
+        if np.issubdtype(data.dtype, np.datetime64):
+            data = data.astype(np.uint64)
+        elif np.issubdtype(data.dtype, np.unicode_):
+            data = data.astype(h5py_string_dtype)
+    else:
+        if np.issubdtype(data.dtype, np.datetime64):
+            data = data.astype(np.uint64)
+        elif np.issubdtype(data.dtype, np.unicode_):
+            data = data.astype(h5py_string_dtype)
+    db.create_dataset(key, data=data)
+    db[key].attrs["dtype"] = data_dtype.str
 
 
 def read_hdf5(filehandle, **kw):
@@ -55,39 +74,37 @@ def read_hdf5(filehandle, **kw):
         db[k] = getvar(filehandle, k)
     return db
 
+if PY3:
+    h5py_string_dtype = h5py.special_dtype(vlen=str)
+else:
+    h5py_string_dtype = h5py.special_dtype(vlen=unicode)
 
-def save_hdf5(db, filehandle, **kw):
+
+def save_hdf5(db, filehandle, expandable=False, **kw):
     filehandle.attrs["hftools file version"] = "0.2"
     for k, v in db.ivardata.items():
-        data = v.data
-        if data.ndim:
-            if data.dtype.name.startswith("datetime64"):
-                data = data.astype(np.uint64)
-            filehandle.create_dataset(k, data=data)
-        else:
-            if data.dtype.name.startswith("datetime64"):
-                data = data.astype(np.uint64)
-            filehandle.create_dataset(k, data=data)
+        create_dataset(filehandle, k, v.data, False)
         filehandle[k].attrs["dimtype"] = v.__class__.__name__
         if v.unit:
             filehandle[k].attrs["unit"] = v.unit
         if v.outputformat:
             filehandle[k].attrs["outputformat"] = v.outputformat
 
-    for k, data in db.vardata.items():
-        if data.ndim:
-            if data.dtype.name.startswith("datetime64"):
-                data = data.astype(np.uint64)
-            filehandle.create_dataset(k, data=data)
-        else:
-            if data.dtype.name.startswith("datetime64"):
-                data = data.astype(np.uint64)
-            filehandle.create_dataset(k, data=data)
-        filehandle[k].attrs["arraytype"] = data.__class__.__name__
-        if data.unit:
-            filehandle[k].attrs["unit"] = data.unit
-        if data.outputformat:
-            filehandle[k].attrs["outputformat"] = data.outputformat
-        for idx, dv in enumerate(data.dims):
+    for k, v in db.vardata.items():
+        create_dataset(filehandle, k, v, False)
+        for idx, dv in enumerate(v.dims):
             filehandle[k].dims.create_scale(filehandle[dv.name])
             filehandle[k].dims[idx].attach_scale(filehandle[dv.name])
+
+        filehandle[k].attrs["arraytype"] = dv.__class__.__name__
+        if v.unit:
+            filehandle[k].attrs["unit"] = v.unit
+        if v.outputformat:
+            filehandle[k].attrs["outputformat"] = v.outputformat
+
+
+def append_hdf5(db, filehandle, **kw):
+    if filehandle.attrs["hftools file version"] != "0.2":
+        raise Exception("Can only append to hftools version 0.2")
+
+
